1f977039d382373eba703231a07190f2c432008f
Dima <strongbaby1337@gmail.com>
2022-04-08 23:41:38 +0300

    app major refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,136 +1,143 @@
 <template>
-  <div class="container" :style="containerStyle">
-    <Cell
-      v-for="(cell, i) in cells"
-      :key="i"
-      :cell="cell"
-      @click="onCellLeftClick(i)"
-      @contextmenu.prevent="onCellRightClick(i)"
-    />
-  </div>
+  <GridView
+    :size="grid.size"
+    :cells="cells"
+    @leftClick="onCellLeftClick"
+    @rightClick="onCellRightClick"
+  />
 </template>
 
 <script>
-import Cell from "@/components/Cell.vue";
-import config from "@/demo/grid.json";
+import GridView from "@/components/GridView"
+import { enumerableRange } from "@/utils/utils.js"
+import { Random } from "@/utils/Random.js"
+import Grid from "@/utils/Grid.js"
+import config from "@/config.json"
 
 export default {
   components: {
-    Cell
+    GridView,
   },
   data() {
     return {
-      grid: {
-        x: 9,
-        y: 9
-      },
+      seed: "123",
+      stage: null,
+      grid: null,
       bombCount: 10,
       cells: [],
     }
   },
+  created() {
+    const params = new URLSearchParams(window.location.search)
+    
+    if (params.has("seed")) {
+      this.seed = params.get("seed")
+    }
+
+    if (params.has("stage")) {
+      const stage = params.get("stage")
+      if (config.stages[stage]) {
+        this.stage = config.stages[stage]
+      }
+      else {
+        this.stage = config.stages.newbie
+      }
+    }
+    else {
+      this.stage = config.stages.newbie
+    }
+
+    this.grid = new Grid(this.stage.size.x, this.stage.size.y)
+  },
   mounted() {
-    this.createGrid();
+    this.createGrid()
   },
   methods: {
+    getRandomSeed() {
+      return (Date.now() % 0xffff).toString(16)
+    },
+    winLogic() {
+      alert("todo: win logic")
+    },
+    loseLogic() {
+      alert("todo: lose logic")
+    },
     createGrid() {
-      const count = this.grid.x * this.grid.y;
+      const count = this.grid.size.x * this.grid.size.y
 
-      // fill empty cells
-      this.cells = new Array(count).fill().map((_, index) => {
+      // ! fill empty cells
+      this.cells = enumerableRange(0, count).map(index => {
         return {
-          index: this.convertTo1d(this.convertTo2d(index)),
-          grid: this.convertTo2d(index),
+          index,
+          grid: this.grid.convertTo2d(index),
           isBomb: false,
           isClicked: false,
           isMarked: false
-        };
-      });
+        }
+      })
 
-      this.fillRandomBombs(count);
-      // this.fillDemoBombs();
+      // ! fill random cells with bombs
+      this.fillRandomBombs()
 
-      // calculate bomb-neighbour count for each cell
+      // ! calculate bomb-neighbour count for each cell
       this.cells.forEach(cell => {
-        cell.count = this.getNeighbours(cell.grid).filter(c => c.isBomb).length;
-      });
+        cell.count = this.grid.getNeighbours(cell.grid)
+          .map(index => this.cells[index])
+          .filter(c => c.isBomb)
+          .length
+      })
     },
-    fillRandomBombs(count) {
-      // fill random cells with bombs
-      const indexes = new Array(count).fill().map((_, index) => index);
-      const candidates = this.shuffleArray(indexes).slice(0, this.bombCount);
+    fillRandomBombs() {
+      const count = this.grid.size.x * this.grid.size.y
+      const indexes = enumerableRange(0, count)
+
+      // ! index of cell where bomb should be placed
+      const candidates = Random.shuffleArray(indexes, this.seed).slice(0, this.stage.bombCount)
+
       candidates.forEach(bombIndex => {
-        this.cells[bombIndex].isBomb = true;
-      });
-    },
-    fillDemoBombs() {
-      config["mines"].forEach(bombIndex => {
-        this.cells[bombIndex].isBomb = true;
-      });
+        this.cells[bombIndex].isBomb = true
+      })
     },
     onCellLeftClick(index) {
-      const cell = this.cells[index];
+      const cell = this.cells[index]
+      if (cell.isMarked) {
+        return
+      }
+
       if (cell.isBomb) {
-        this.cells.filter(c => c.isBomb).forEach(c => c.isClicked = true);
+        this.cells.filter(c => c.isBomb).forEach(c => c.isClicked = true)
+        this.loseLogic()
       }
       else {
-        this.revealCell(cell);
+        this.revealCell(cell)
+
+        const revealedLength = this.cells.filter(cell => cell.isClicked).length
+        const win = revealedLength === this.cells.length - this.stage.bombCount
+        if (win) {
+          this.winLogic()
+        }
       }
     },
     onCellRightClick(index) {
-      const cell = this.cells[index];
+      const cell = this.cells[index]
       if (!cell.isClicked) {
-        cell.isMarked = !cell.isMarked;
+        cell.isMarked = !cell.isMarked
+        // ! win if all bomb cells is marked
+        const markedBombsCount = this.cells.filter(cell => cell.isBomb && cell.isMarked).length
+        const win = markedBombsCount === this.stage.bombCount
+        if (win) {
+          this.winLogic()
+        }
       }
     },
     revealCell(cell) {
-      if (cell.isClicked) return;
-      cell.isClicked = true;
-      const neighbours = this.getNeighbours(cell.grid);
-      const mines = neighbours.filter(c => c.isBomb);
-      if (mines.length === 0) neighbours.forEach(c => this.revealCell(c));
-    },
-    convertTo2d(index) {
-      return {
-        x: index % this.grid.x,
-        y: Math.floor(index / this.grid.y)
+      if (cell.isClicked) {
+        return
       }
-    },
-    convertTo1d({ x, y }) {
-      return y * this.grid.x + x;
-    },
-    getNeighbours({ x, y }) {
-      const cells = [];
-      for (let _x = -1; _x <= 1; _x++) {
-        for (let _y = -1; _y <= 1; _y++) {
-          const pos = { x: x + _x, y: y + _y };
-          if (_x === 0 && _y === 0) continue;
-          if (this.isInside(pos)) {
-            cells.push(this.cells[this.convertTo1d(pos)]);
-          }
-        }
-      }
-      return cells;
-    },
-    isInside({ x, y }) {
-      return x >= 0 && x < this.grid.x && y >= 0 && y < this.grid.y;
-    },
-    shuffleArray(array) {
-      const arr = [...array];
-      for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-      return arr;
-    }
-  },
-  computed: {
-    containerStyle() {
-      return {
-        "--grid-x": this.grid.x,
-        "--grid-y": this.grid.y
-      }
+      cell.isClicked = true
+      const neighbours = this.grid.getNeighbours(cell.grid).map(index => this.cells[index])
+      const mines = neighbours.filter(c => c.isBomb)
+      if (mines.length === 0) neighbours.forEach(c => this.revealCell(c))
     },
   },
 }
@@ -139,18 +146,5 @@ export default {
 <style>
 body {
   user-select: none;
-}
-
-.container {
-  display: grid;
-  grid-template-columns: repeat(var(--grid-x), 50px);
-  grid-template-rows: repeat(var(--grid-y), 50px);
-  grid-gap: 5px;
-}
-
-.cell {
-  border: 1px solid black;
-  font-size: 20px;
-  text-align: center;
 }
 </style>
